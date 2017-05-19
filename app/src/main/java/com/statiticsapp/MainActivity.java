191529179fb4fba1f96b97f2cbf93f8fe8231903
Nelson Gerardo Peltzer <nelson.peltzer@gmail.com>
2017-05-19 15:12:15 -0300

    Make of tabs

--- app/src/main/java/com/statiticsapp/MainActivity.java
+++ app/src/main/java/com/statiticsapp/MainActivity.java
@@ -5,41 +5,49 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    TabHost mainTabHost;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private static String CALCULAR = "tab1";
+    private static String GRAFICA = "tab2";
+    private static String TEORIA = "tab3";
+    private static int CALCULAR_TAB = 0;
+    private static int GRAFICA_TAB = 1;
+    private static int TEORIA_TAB = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Main Tab Host
+        mainTabHost = (TabHost) findViewById(R.id.activity_main_tab_host);
+        mainTabHost.setup();
+
+        // Tab Calcular
+        TabHost.TabSpec spec = mainTabHost.newTabSpec(CALCULAR);
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Calcular");
+        mainTabHost.addTab(spec);
+
+        // Tab Gráfica
+        spec = mainTabHost.newTabSpec(GRAFICA);
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Gráfica");
+        mainTabHost.addTab(spec);
+
+        // Tab Teoría
+        spec = mainTabHost.newTabSpec(TEORIA);
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Teoría");
+        mainTabHost.addTab(spec);
+
+        mainTabHost.setCurrentTab(CALCULAR_TAB);
     }
 
 }
