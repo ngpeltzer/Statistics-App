aed3527a7b2941a6983289ea2b5f37b17a55eeff
Nelson Gerardo Peltzer <nelson.peltzer@gmail.com>
2017-10-13 11:51:23 -0300

    Added Excel support + Standard Normal Distribution

---
+++ app/src/main/java/com/statiticsapp/Utils/Constants.java
@@ -0,0 +1,10 @@
+package com.statiticsapp.Utils;
+
+public class Constants {
+
+    /* DIALOG CONSTANTS */
+    public static final String MESSAGE = "message";
+    public static final String BUTTON_OK = "button_ok";
+    public static final String BUTTON_CANCEL = "button_cancel";
+    public static final String TITLE = "title";
+}
