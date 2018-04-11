8b0c9307ff498a9bf2e2299a2515d560c15a1fe5
Nelson Gerardo Peltzer <nelson.peltzer@gmail.com>
2018-04-11 20:08:22 -0300

    Fix java.lang.NoClassDefFoundError crashes

--- app/src/main/java/com/statisticsapp/StatisticsApplication.java
+++ app/src/main/java/com/statisticsapp/StatisticsApplication.java
@@ -2,11 +2,12 @@ package com.statisticsapp;
 
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-public class StatisticsApplication extends Application {
+public class StatisticsApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
